8242c515165eaba40a5f20bb3233fd19fef44ef2
Tiago Garcia <tiago.rgarcia@ua.pt>
2024-07-04 23:23:30 +0100

    Implement caderno
    Signed-off-by: Tiago Garcia <tiago.rgarcia@ua.pt>

--- src/deploy-commands.ts
+++ src/deploy-commands.ts
@@ -7,62 +7,83 @@ const clientId = process.env.CLIENT_ID;
 const guildId = process.env["COMMANDS_GUILD_ID"];
 
 if (!token || !clientId) {
-	console.error("Missing environment variables. Please check your .env file.");
-	process.exit(1);
+    console.error("Missing environment variables. Please check your .env file.");
+    process.exit(1);
 }
 
 const commands = [
-	new SlashCommandBuilder().setName('ping').setDescription('Check the bot ping'),
+    new SlashCommandBuilder().setName('ping').setDescription('Check the bot ping'),
 
-	new SlashCommandBuilder().setName('nome-de-faina').setDescription('Efetua o pedido de mudança de nome de faina no servidor')
-		.addStringOption(option => option.setName('nome').setDescription('Novo nome de faina').setRequired(true)),
+    new SlashCommandBuilder().setName('nome-de-faina').setDescription('Efetua o pedido de mudança de nome de faina no servidor')
+        .addStringOption(option => option.setName('nome').setDescription('Novo nome de faina').setRequired(true)),
 
     new SlashCommandBuilder().setName('completar-faina').setDescription('Marca/desmarcar a faina de um utilizador como completa')
         .addUserOption(option => option.setName('utilizador').setDescription('Utilizador cuja faina será marcada/desmarcada como completa').setRequired(true))
-		.setDefaultMemberPermissions(0),
+        .setDefaultMemberPermissions(0),
 
     new SlashCommandBuilder().setName('numero-aluviao').setDescription('Define o número de aluvião de um utilizador')
         .addUserOption(option => option.setName('utilizador').setDescription('Utilizador cujo número de aluvião será alterado').setRequired(true))
         .addStringOption(option => option.setName('numero').setDescription('Novo número de aluvião').setRequired(true))
-		.setDefaultMemberPermissions(0),
+        .setDefaultMemberPermissions(0),
 
-	new SlashCommandBuilder().setName('edit-user').setDescription('Edita os dados de um utilizador')
-		.addUserOption(option => option.setName('utilizador').setDescription('Utilizador a editar').setRequired(true))
-		.setDefaultMemberPermissions(0),
+    new SlashCommandBuilder().setName('caderno').setDescription('Controla o caderno')
+        .addSubcommand(subcommand =>
+            subcommand.setName('get').setDescription('Mostra o caderno atual')
+        )
+        .addSubcommand(subcommand =>
+            subcommand.setName('reset').setDescription('Reinicia o caderno')
+        )
+        .addSubcommand(subcommand =>
+            subcommand.setName('set').setDescription('Define o valor do caderno')
+                .addIntegerOption(option => option.setName('amount').setDescription('Novo valor do caderno').setRequired(true))
+        )
+        .addSubcommand(subcommand =>
+            subcommand.setName('add').setDescription('Adiciona traços ao caderno')
+                .addIntegerOption(option => option.setName('amount').setDescription('Valor a adicionar ao caderno').setRequired(true))
+        )
+        .addSubcommand(subcommand =>
+            subcommand.setName('remove').setDescription('Remove traços do caderno')
+                .addIntegerOption(option => option.setName('amount').setDescription('Valor a remover ao caderno').setRequired(true))
+        )
+        .setDefaultMemberPermissions(0),
 
-	new SlashCommandBuilder().setName('user-info').setDescription('Consulta os dados de um utilizador')
-		.addUserOption(option => option.setName('utilizador').setDescription('Utilizador a consultar').setRequired(true))
-		.setDefaultMemberPermissions(0),
+    new SlashCommandBuilder().setName('edit-user').setDescription('Edita os dados de um utilizador')
+        .addUserOption(option => option.setName('utilizador').setDescription('Utilizador a editar').setRequired(true))
+        .setDefaultMemberPermissions(0),
 
-	new SlashCommandBuilder().setName('send-verify-message').setDescription('Sends the message to setup the verification channel')
-		.setDefaultMemberPermissions(0),
+    new SlashCommandBuilder().setName('user-info').setDescription('Consulta os dados de um utilizador')
+        .addUserOption(option => option.setName('utilizador').setDescription('Utilizador a consultar').setRequired(true))
+        .setDefaultMemberPermissions(0),
 
-	new SlashCommandBuilder().setName('run-db').setDescription('Runs a command on the database')
-		.addSubcommand(subcommand =>
-			subcommand.setName('get').setDescription('Gets data from tables on the database')
-				.addStringOption(option => option.setName('query').setDescription('Query to run on the database').setRequired(true))
-		)
-		.addSubcommand(subcommand =>
-			subcommand.setName('run').setDescription('Runs a command on the database')
-				.addStringOption(option => option.setName('query').setDescription('Query to run on the database').setRequired(true))
-		)
-		.setDefaultMemberPermissions(0),
+    new SlashCommandBuilder().setName('send-verify-message').setDescription('Sends the message to setup the verification channel')
+        .setDefaultMemberPermissions(0),
 
-	new ContextMenuCommandBuilder().setName('Completar faina').setType(ApplicationCommandType.User)
-		.setDefaultMemberPermissions(0).setDMPermission(false),
+    new SlashCommandBuilder().setName('run-db').setDescription('Runs a command on the database')
+        .addSubcommand(subcommand =>
+            subcommand.setName('get').setDescription('Gets data from tables on the database')
+                .addStringOption(option => option.setName('query').setDescription('Query to run on the database').setRequired(true))
+        )
+        .addSubcommand(subcommand =>
+            subcommand.setName('run').setDescription('Runs a command on the database')
+                .addStringOption(option => option.setName('query').setDescription('Query to run on the database').setRequired(true))
+        )
+        .setDefaultMemberPermissions(0),
 
-	new ContextMenuCommandBuilder().setName('Número de aluvião').setType(ApplicationCommandType.User)
-		.setDefaultMemberPermissions(0).setDMPermission(false),
+    new ContextMenuCommandBuilder().setName('Completar faina').setType(ApplicationCommandType.User)
+        .setDefaultMemberPermissions(0).setDMPermission(false),
+
+    new ContextMenuCommandBuilder().setName('Número de aluvião').setType(ApplicationCommandType.User)
+        .setDefaultMemberPermissions(0).setDMPermission(false),
 ].map(command => command.toJSON());
 
 const rest = new REST({ version: '10' }).setToken(token!);
 
 if (guildId) {
-	rest.put(Routes.applicationGuildCommands(clientId!, guildId), { body: commands })
-		.then((data: any) => console.log(`Successfully registered ${data.length} application commands on guild ${guildId}.`))
-		.catch(console.error);
+    rest.put(Routes.applicationGuildCommands(clientId!, guildId), { body: commands })
+        .then((data: any) => console.log(`Successfully registered ${data.length} application commands on guild ${guildId}.`))
+        .catch(console.error);
 } else {
-	rest.put(Routes.applicationCommands(clientId!), { body: commands })
-		.then((data: any) => console.log(`Successfully registered ${data.length} global application commands.`))
-		.catch(console.error);
+    rest.put(Routes.applicationCommands(clientId!), { body: commands })
+        .then((data: any) => console.log(`Successfully registered ${data.length} global application commands.`))
+        .catch(console.error);
 }
---
+++ src/discord/commands/caderno.ts
@@ -0,0 +1,46 @@
+import { type ChatInputCommandInteraction } from "discord.js";
+import { Command } from "../registry/Command";
+import Database from "../../Database";
+
+export default class CadernoCommand extends Command {
+    constructor() {
+        super("caderno", "Controla o caderno");
+    }
+
+    public async execute(interaction: ChatInputCommandInteraction) {
+        await interaction.deferReply({ });
+
+        const currentCaderno = await Database.get("SELECT * FROM FainaData WHERE Key = ?", ["caderno"]);
+        let caderno = 0;
+        if (!currentCaderno)
+            await Database.run("INSERT INTO FainaData (Key, Value) VALUES (?, ?)", ["caderno", "0"]);
+        else
+            caderno = parseInt(currentCaderno.Value);
+
+        const subcommand = interaction.options.getSubcommand(true);
+        if (subcommand === "get")
+            await interaction.editReply({ content: `O caderno está em **${caderno}** traços!` });
+        else if (subcommand === "reset") {
+            await Database.run("UPDATE FainaData SET Value = ? WHERE Key = ?", [caderno.toString(), "caderno"]);
+            await interaction.editReply({ content: "O caderno foi reiniciado!" });
+        }
+        else if (subcommand === "set") {
+            const amount = interaction.options.getInteger("amount", true);
+            await Database.run("UPDATE FainaData SET Value = ? WHERE Key = ?", [amount.toString(), "caderno"]);
+            await interaction.editReply({ content: `O caderno foi definido para **${amount}** traços!` });
+        }
+        else if (subcommand === "add") {
+            const amount = interaction.options.getInteger("amount", true);
+            caderno += amount;
+            await Database.run("UPDATE FainaData SET Value = ? WHERE Key = ?", [caderno.toString(), "caderno"]);
+            await interaction.editReply({ content: `Foram adicionados **${amount}** traços ao caderno!\nO caderno está agora em **${caderno}** traços!`});
+        }
+        else if (subcommand === "remove") {
+            const amount = interaction.options.getInteger("amount", true);
+            caderno -= amount;
+            if (caderno < 0) caderno = 0;
+            await Database.run("UPDATE FainaData SET Value = ? WHERE Key = ?", [caderno.toString(), "caderno"]);
+            await interaction.editReply({ content: `Foram removidos **${amount}** traços ao caderno!\nO caderno está agora em **${caderno}** traços!` });
+        }
+    }
+}
