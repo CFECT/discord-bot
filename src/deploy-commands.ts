cf17552ff92e008c0ef9e1ed7ee9d5ea1c58e771
Tiago Garcia <tiago.rgarcia@ua.pt>
2024-08-26 21:09:36 +0100

    Set some global commands
    Signed-off-by: Tiago Garcia <tiago.rgarcia@ua.pt>

--- src/deploy-commands.ts
+++ src/deploy-commands.ts
@@ -4,20 +4,40 @@ require("dotenv").config();
 
 const token = process.env.DISCORD_BOT_TOKEN;
 const clientId = process.env.CLIENT_ID;
-const guildId = process.env["COMMANDS_GUILD_ID"];
+const guildId = process.env.COMMANDS_GUILD_ID;
 
 if (!token || !clientId) {
     console.error("Missing environment variables. Please check your .env file.");
     process.exit(1);
 }
 
-const commands = [
+const global_commands = [
     new SlashCommandBuilder().setName('ping').setDescription('Check the bot ping'),
 
     new SlashCommandBuilder().setName('relembrar').setDescription('Relembrar-te de algo')
         .addStringOption(option => option.setName('time').setDescription('Tempo para relembrar-te').setRequired(true))
         .addStringOption(option => option.setName('message').setDescription('Mensagem a relembrar-te').setRequired(true)),
 
+    new SlashCommandBuilder().setName('eval').setDescription('Evaluates javascript code')
+        .addStringOption(option => option.setName('expression').setDescription('Expression to evaluate').setRequired(true))
+        .addBooleanOption(option => option.setName('async').setDescription('Whether to evaluate the expression asynchronously'))
+        .addBooleanOption(option => option.setName('silent').setDescription('Whether to suppress the output'))
+        .addIntegerOption(option => option.setName('depth').setDescription('Depth of the output'))
+        .setDefaultMemberPermissions(0),
+
+    new SlashCommandBuilder().setName('run-db').setDescription('Runs a command on the database')
+        .addSubcommand(subcommand =>
+            subcommand.setName('get').setDescription('Gets data from tables on the database')
+                .addStringOption(option => option.setName('query').setDescription('Query to run on the database').setRequired(true))
+        )
+        .addSubcommand(subcommand =>
+            subcommand.setName('run').setDescription('Runs a command on the database')
+                .addStringOption(option => option.setName('query').setDescription('Query to run on the database').setRequired(true))
+        )
+        .setDefaultMemberPermissions(0),
+].map(command => command.toJSON());
+
+const guild_commands = [
     new SlashCommandBuilder().setName('nome-de-faina').setDescription('Efetua o pedido de mudança de nome de faina no servidor')
         .addStringOption(option => option.setName('nome').setDescription('Novo nome de faina').setRequired(true)),
 
@@ -69,24 +89,6 @@ const commands = [
         .addUserOption(option => option.setName('utilizador').setDescription('Utilizador a consultar').setRequired(true))
         .setDefaultMemberPermissions(0),
 
-    new SlashCommandBuilder().setName('eval').setDescription('Evaluates javascript code')
-        .addStringOption(option => option.setName('expression').setDescription('Expression to evaluate').setRequired(true))
-        .addBooleanOption(option => option.setName('async').setDescription('Whether to evaluate the expression asynchronously'))
-        .addBooleanOption(option => option.setName('silent').setDescription('Whether to suppress the output'))
-        .addIntegerOption(option => option.setName('depth').setDescription('Depth of the output'))
-        .setDefaultMemberPermissions(0),
-
-    new SlashCommandBuilder().setName('run-db').setDescription('Runs a command on the database')
-        .addSubcommand(subcommand =>
-            subcommand.setName('get').setDescription('Gets data from tables on the database')
-                .addStringOption(option => option.setName('query').setDescription('Query to run on the database').setRequired(true))
-        )
-        .addSubcommand(subcommand =>
-            subcommand.setName('run').setDescription('Runs a command on the database')
-                .addStringOption(option => option.setName('query').setDescription('Query to run on the database').setRequired(true))
-        )
-        .setDefaultMemberPermissions(0),
-
     new ContextMenuCommandBuilder().setName('Completar faina').setType(ApplicationCommandType.User)
         .setDefaultMemberPermissions(0).setDMPermission(false),
 
@@ -96,12 +98,12 @@ const commands = [
 
 const rest = new REST({ version: '10' }).setToken(token!);
 
+rest.put(Routes.applicationCommands(clientId!), { body: global_commands })
+    .then((data: any) => console.log(`Successfully registered ${data.length} global application commands.`))
+    .catch(console.error);
+
 if (guildId) {
-    rest.put(Routes.applicationGuildCommands(clientId!, guildId), { body: commands })
-        .then((data: any) => console.log(`Successfully registered ${data.length} application commands on guild ${guildId}.`))
-        .catch(console.error);
-} else {
-    rest.put(Routes.applicationCommands(clientId!), { body: commands })
-        .then((data: any) => console.log(`Successfully registered ${data.length} global application commands.`))
-        .catch(console.error);
+    rest.put(Routes.applicationGuildCommands(clientId!, guildId), { body: guild_commands })
+    .then((data: any) => console.log(`Successfully registered ${data.length} application commands on guild ${guildId}.`))
+    .catch(console.error);
 }
