1293d2a7aada8400e6a1e6f707f564233c35bb68
Francisco Ribeiro <francisco.ribeiro@ua.pt>
2024-10-06 18:54:48 +0100

    Add embeds with progress when completing faina or updating número de aluvião Fix an error where updating year would remove the Aluvião role and give the Veterano role to everyone. This doesn't apply to everyone.
    Signed-off-by: Francisco Ribeiro <francisco.ribeiro@ua.pt>

--- src/discord/commands/completar-faina-bulk.ts
+++ src/discord/commands/completar-faina-bulk.ts
@@ -1,7 +1,6 @@
-import { Attachment, CommandInteraction, GuildMember, InteractionReplyOptions, Team } from "discord.js";
+import { Attachment, CommandInteraction, EmbedBuilder, Team } from "discord.js";
 import { Command } from "../registry/Command";
 import Constants from "../../Constants";
-import Database from "../../Database";
 import Utils from "../../Utils";
 
 export default class CompletarFainaBulkCommand extends Command {
@@ -34,7 +33,7 @@ export default class CompletarFainaBulkCommand extends Command {
             }
         }
 
-        await interaction.editReply({ content: "Processing file..." });
+        await interaction.editReply({ content: "File added to processing." });
 
         const fileAtt = interaction.options.get('ficheiro', true).attachment as Attachment;
         const fileUrl = fileAtt.url;
@@ -45,61 +44,20 @@ export default class CompletarFainaBulkCommand extends Command {
             return;
         }
 
-        const invalid_lines: string[] = [];
-        const not_found: string[] = [];
-        const processing_errors: string[] = [];
-
         const text = await file.text();
-        const lines = text.split('\n');
-        lines.pop();
-        const query = "UPDATE Users SET FainaCompleta = true WHERE NMec = ?";
-        for (const index in lines) {
-            const nmec = lines[index];
-            if (!nmec || nmec.length <= 4 || nmec.length >= 7 || isNaN(parseInt(nmec))) {
-                invalid_lines.push(`- Line ${parseInt(index) + 1} - ${nmec}`);
-                continue;
-            }
 
-            const user = await Database.get("SELECT * FROM Users WHERE NMec = ?", [nmec]);
-            if (!user) {
-                not_found.push(`- ${nmec}`);
-                continue;
-            }
-            const discordUserId = user.DiscordID;
+        const embed = new EmbedBuilder()
+            .setTitle("Completar Faina")
+            .setDescription("A marcar a faina de utilizadores como completa...")
+            .setFields(
+                { name: "Número Mecanográfico", value: "Pending...", inline: true },
+                { name: "Utilizador", value: "Pending...", inline: true },
+                { name: "Progresso", value: "Pending...", inline: true }
+            )
+            .setColor(Constants.EMBED_COLORS.UPDATE_IN_PROGRESS)
+            .setTimestamp(Date.now());
 
-            await Database.run(query, [nmec]).then(async () => {
-                const member = await interaction.guild?.members.fetch(discordUserId as string) as GuildMember;
-                await member.roles.remove(Constants.ROLES.ALUVIAO);
-                await member.roles.add(Constants.ROLES.VETERANO);
-                await Utils.updateNickname(member);
-                await new Promise(resolve => setTimeout(resolve, 1000));
-            }).catch((error) => {
-                processing_errors.push(`- Line ${parseInt(index) + 1} - ${nmec} - ${error}`);
-            });
-        }
-
-        if (invalid_lines.length === 0 && not_found.length === 0 && processing_errors.length === 0) {
-            await interaction.editReply({ content: "Operation finished successfully." });
-            return;
-        }
-
-        let message = "";
-        if (invalid_lines.length > 0)
-            message += `## Invalid lines:\n${invalid_lines.join("\n")}\n\n`;
-        if (not_found.length > 0)
-            message += `## Users not found on the database:\n${not_found.join("\n")}\n\n`;
-        if (processing_errors.length > 0)
-            message += `## Errors processing lines:\n${processing_errors.join("\n")}`;
-
-        const messageToSend: InteractionReplyOptions = {
-            content: "Operation finished with errors.",
-            ephemeral: true,
-            files: [{
-                attachment: Buffer.from(message),
-                name: "errors.md"
-            }]
-        }
-
-        await interaction.editReply(messageToSend);
+        const message = await interaction.channel!.send({ embeds: [embed] });
+        await Utils.completarFainaBulk(message, text);
     }
 }
--- src/discord/commands/numero-aluviao-bulk.ts
+++ src/discord/commands/numero-aluviao-bulk.ts
@@ -1,7 +1,7 @@
-import { Attachment, CommandInteraction, GuildMember, InteractionReplyOptions, Team } from "discord.js";
+import { Attachment, CommandInteraction, EmbedBuilder, Team } from "discord.js";
 import { Command } from "../registry/Command";
-import Database from "../../Database";
 import Utils from "../../Utils";
+import Constants from "../../Constants";
 
 export default class NumeroAluviaoBulkCommand extends Command {
     constructor() {
@@ -33,7 +33,7 @@ export default class NumeroAluviaoBulkCommand extends Command {
             }
         }
 
-        await interaction.editReply({ content: "Processing file..." });
+        await interaction.editReply({ content: "File added to processing." });
 
         const fileAtt = interaction.options.get('ficheiro', true).attachment as Attachment;
         const fileUrl = fileAtt.url;
@@ -44,62 +44,20 @@ export default class NumeroAluviaoBulkCommand extends Command {
             return;
         }
 
-        const invalid_lines: string[] = [];
-        const not_found: string[] = [];
-        const processing_errors: string[] = [];
-
         const text = await file.text();
-        const lines = text.split('\n');
-        const query = "UPDATE Users SET NumeroAluviao = ? WHERE NMec = ?";
-        for (const index in lines) {
-            const line = lines[index];
-            if (!line) continue;
-            let [nmec, naluviao] = line.split(',');
-            if (!nmec || !naluviao) {
-                invalid_lines.push(`- Line ${parseInt(index) + 1} - ${line}`);
-                continue;
-            }
 
-            const user = await Database.get("SELECT * FROM Users WHERE NMec = ?", [nmec]);
-            if (!user) {
-                not_found.push(`- ${nmec}`);
-                continue;
-            }
-            const discordUserId = user.DiscordID;
+        const embed = new EmbedBuilder()
+            .setTitle("Atualização de Números de Aluvião")
+            .setDescription("A atualizar números de aluvião...")
+            .setFields(
+                { name: "Número Mecanográfico", value: "Pending...", inline: true },
+                { name: "Utilizador", value: "Pending...", inline: true },
+                { name: "Progresso", value: "Pending...", inline: true }
+            )
+            .setColor(Constants.EMBED_COLORS.UPDATE_IN_PROGRESS)
+            .setTimestamp(Date.now());
 
-            await Database.run(query, [naluviao, nmec]).then(async () => {
-                if (!user.FainaCompleta) {
-                    const member = await interaction.guild?.members.fetch(discordUserId as string) as GuildMember;
-                    await Utils.updateNickname(member);
-                    await new Promise(resolve => setTimeout(resolve, 1000));
-                }
-            }).catch((error) => {
-                processing_errors.push(`- Line ${parseInt(index) + 1} - ${line} - ${error}`);
-            });
-        }
-
-        if (invalid_lines.length === 0 && not_found.length === 0 && processing_errors.length === 0) {
-            await interaction.editReply({ content: "Operation finished successfully." });
-            return;
-        }
-
-        let message = "";
-        if (invalid_lines.length > 0)
-            message += `## Invalid lines:\n${invalid_lines.join("\n")}\n\n`;
-        if (not_found.length > 0)
-            message += `## Users not found on the database:\n${not_found.join("\n")}\n\n`;
-        if (processing_errors.length > 0)
-            message += `## Errors processing lines:\n${processing_errors.join("\n")}`;
-
-        const messageToSend: InteractionReplyOptions = {
-            content: "Operation finished with errors.",
-            ephemeral: true,
-            files: [{
-                attachment: Buffer.from(message),
-                name: "errors.md"
-            }]
-        }
-
-        await interaction.editReply(messageToSend);
+        const message = await interaction.channel!.send({ embeds: [embed] });
+        await Utils.updateNumerosAluviaoBulk(message, text);
     }
 }
